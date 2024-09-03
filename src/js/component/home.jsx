d64213bda6f5cce3e84a5166a77d64214710399c
Joni Santos <jonisantos1996@hotmail.com>
2024-09-03 22:05:17 +0000

    todolist completed

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,96 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+
+	const [ list, setList ] = useState([]);
+	const [ task, setTask ] = useState("");
+
+	const host = 'https://playground.4geeks.com';
+
+	const getData = async () => {
+		const uri = `${host}/todo/users/JoniXSantos`;
+		const response = await fetch(uri);
+		if (!response.ok) {
+			console.log('Error: ', response.status, response.statusText);
+			return;
+		};
+		const data = await response.json();
+		setList(data.todos);
+	}
+
+	const addTask = async () => {
+		const dataToSend = {
+			label: task,
+			is_done: false
+		};
+		const uri = `${host}/todo/todos/JoniXSantos`;
+		const options = {
+			method: 'POST',
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(dataToSend)
+		};
+		const response = await fetch(uri, options);
+		if (!response.ok) {
+			console.log('Error: ', response.status, response.statusText);
+			return;
+		};
+		setTask('');
+		getData();
+	}
+
+	const deleteTask = async (item) => {
+		const uri = `${host}/todo/todos/${item.id}`;
+		const options = {
+			method: 'DELETE',
+			headers: {
+				"Content-Type": "application/json"
+			}
+		};
+		const response = await fetch(uri, options);
+		if (!response.ok) {
+			console.log('Error: ', response.status, response.statusText);
+			return;
+		}
+		getData();
+	}
+
+	useEffect(() => {
+		getData();
+	}, []);
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container col-4">
+			<h1 className="text-center text-light mt-3">To Do List</h1>
+			<h2 className="text-center text-light mb-3">with React and Fetch</h2>
+			<ul>
+				<li>
+					<input
+						type="text"
+						value={task}
+						onChange={(e) => setTask(e.target.value)}
+						onKeyDown={(e) => {
+							if (e.key === "Enter") {
+								addTask();
+							}}}
+						placeholder="Add a task"
+					/>
+				</li>
+				{list.map((item) => (
+					<li key={item.id}>
+						<div className="d-flex align-items-baseline justify-content-between">
+							{item.label}
+							<i className="fas fa-trash text-danger" 
+						   	onClick={() => deleteTask(item)}>
+							</i>
+						</div>
+					</li>
+				))}
+				<li style={{fontSize: "13px"}}>
+					{list.length} {list.length !== 1 ? "tasks" : "task"} left
+				</li>
+			</ul>
 		</div>
 	);
 };
