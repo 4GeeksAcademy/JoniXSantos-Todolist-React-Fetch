2f4e4ca0891e011596fc832aa2c908110a27c927
SrZeroRequiem <funkymnks@gmail.com>
2024-09-06 20:40:38 +0200

    Revision

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -11,6 +11,7 @@ const Home = () => {
 		const uri = `${host}/todo/users/JoniXSantos`;
 		const response = await fetch(uri);
 		if (!response.ok) {
+			if(response.status === 404) return createUser()
 			console.log('Error: ', response.status, response.statusText);
 			return;
 		};
@@ -54,6 +55,19 @@ const Home = () => {
 			return;
 		}
 		getData();
+	}
+
+	const createUser = async() =>{
+		const uri = `${host}/todo/users/JoniXSantos`
+		const options = {
+			method: 'POST'
+		}
+		const response = await fetch(uri,options)
+
+		if(!response.ok) console.log(response.status)
+
+		getData()
+
 	}
 
 	useEffect(() => {
